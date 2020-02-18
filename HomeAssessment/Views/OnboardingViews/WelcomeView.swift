524f171364ae0e99a3110ed0f9ba426abc4079d4
mengoreo <mengoreo@163.com>
2020-02-18 16:14:49 +0800

    Remove sign up view
    animation improvements

--- HomeAssessment/Views/OnboardingViews/WelcomeView.swift
+++ HomeAssessment/Views/OnboardingViews/WelcomeView.swift
@@ -12,29 +12,31 @@ import SwiftUI
 struct WelcomeView: View {
     @ObservedObject var viewModel: OnboardingViewModel
 
-    @State private var iconOffset: CGFloat = -100
-    @State private var buttonOffset: CGFloat = -100
-    private var signInView: SignInView
-//    private var signUpView: SignUpView
-    
+    private var signInUpView: SignInUpView
+    private var assessmentListView: AssessmentListView
     init(viewModel: OnboardingViewModel) {
         self.viewModel = viewModel
-        self.signInView = SignInView(viewModel: .init(from: viewModel))
+        self.signInUpView = SignInUpView(viewModel: .init(from: viewModel))
+        self.assessmentListView = AssessmentListView(viewModel: .init())
     }
     
     var body: some View {
-        ZStack {
+        ZStack {           
+            
             VStack {
                 ICON()
-                if viewModel.signInViewModal {
-                    signInView
+                if viewModel.signInUpViewModal {
+                    signInUpView
                 }
-//                signInView.frame(width: 0, height: 0, alignment: .center)
                 BUTTONS()
             }
-            AssessmentListView()
-                .opacity(self.viewModel.displayHome ? 0 : 0)
-                .animation(.linear)
+            
+            assessmentListView
+            .opacity(viewModel.displayHome ? 1 : 0)
+            .animation(.myease)
+            
+        }.onAppear{
+            self.viewModel.showWelcom()
         }
     }
 }
@@ -42,42 +44,33 @@ struct WelcomeView: View {
 extension WelcomeView {
     func BUTTONS() -> some View {
         return VStack {
-            if !self.viewModel.displayHome {
-                Spacer()
-            }
+            Spacer()
             HStack {
-                Button(action: {
-                    
-                    if self.viewModel.signInViewModal {
+                if viewModel.signInUpViewModal {
+                    Button(action: {
                         self.viewModel.showWelcom()
-                    } else {
-                        self.viewModel.showSignUpView()
-                    }
-                    self.viewModel.updateIcon()
-                }) {
-                    HStack {
-                        if self.viewModel.signInViewModal {
-                            Image(systemName: "chevron.right").font(.title).frame(height: 30)
-                        } else {
-                            Text("注册")
-                                .fontWeight(.semibold)
-                                .font(.title)
-                        }
-                    }
-                                        
-                }.buttonStyle(CollapsableOutlineBackgroundStyle(collapsed: viewModel.signInViewModal, xOffset: viewModel.signInViewModal ? 20 : 0))
+                        self.viewModel.updateView()
+                    }) {
+                        Image(systemName: "chevron.left").font(.title).frame(height: 30)
+                        
+                    }.padding(.leading, 20)
+                    .buttonStyle(CollapsableOutlineBackgroundStyle(collapsed: true))
+                }
                 
                 Button(action: {
 //                    self.viewModel.showSignInView()
-                    if self.viewModel.signInViewModal {
-                        self.signInView.viewModel.authorising()
-                        self.viewModel.updateIcon()
+                    if self.viewModel.signInUpViewModal {
+//                        self.signInUpView.viewModel.authorising()
+                        self.viewModel.authorised = true
+                        self.viewModel.showWelcom()
+                        self.viewModel.updateView()
+                        self.assessmentListView.viewModel.readyForDisplay()
                     } else {
-                        self.viewModel.showSignInView()
-                        self.viewModel.updateIcon()
+                        self.viewModel.showSignInUpView()
+                        self.viewModel.updateView()
                     }
                 }) {
-                    Text("登录")
+                    Text("登录/注册")
                         .fontWeight(.semibold)
                         .font(.title)
                 }
@@ -85,10 +78,7 @@ extension WelcomeView {
                 
             }
         }
-        .padding(.bottom, buttonOffset).animation(.myease)
-        .onAppear{
-            self.buttonOffset = 23
-        }
+        .padding(.bottom, viewModel.uiProperties.buttonBottomPadding).animation(.myease)
     }
     
     func ICON() -> some View {
@@ -96,15 +86,14 @@ extension WelcomeView {
         Image("icon")
             .resizable()
             .frame(
-                width: viewModel.iconProperties.frame.width,
-                height: viewModel.iconProperties.frame.height, alignment: .center)
-            .padding(.top, viewModel.iconProperties.topPadding)
-            .padding(.bottom, viewModel.iconProperties.bottomPadding)
-            .opacity(viewModel.iconProperties.opacity)
+                width: viewModel.uiProperties.iconFrame.width,
+                height: viewModel.uiProperties.iconFrame.height, alignment: .center)
+            .padding(.top, viewModel.uiProperties.iconTopPadding)
+            .padding(.bottom, viewModel.uiProperties.iconBottomPadding)
+            .padding(.trailing, viewModel.uiProperties.iconTrailingPadding)
+            .opacity(viewModel.uiProperties.iconOpacity)
             .animation(.myease)
-            .onAppear{
-                self.viewModel.updateIcon()
-            }
+            
             
         }
 
