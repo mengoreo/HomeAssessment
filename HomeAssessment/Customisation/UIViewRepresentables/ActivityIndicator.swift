ce25d432a7a07e173181dbdc485825863dfdc48a
mengoreo <mengoreo@163.com>
2020-02-29 22:31:01 +0800

    add animation start signal

--- HomeAssessment/Customisation/UIViewRepresentables/ActivityIndicator.swift
+++ HomeAssessment/Customisation/UIViewRepresentables/ActivityIndicator.swift
@@ -12,12 +12,18 @@ struct ActivityIndicator: UIViewRepresentable {
 
     @Binding var isAnimating: Bool
     let style: UIActivityIndicatorView.Style
-
+    var animationStarted: () -> Void = {}
     func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
         return UIActivityIndicatorView(style: style)
     }
 
     func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
-        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+        if isAnimating {
+            uiView.startAnimating()
+            animationStarted()
+        } else {
+            uiView.stopAnimating()
+            
+        }
     }
 }
