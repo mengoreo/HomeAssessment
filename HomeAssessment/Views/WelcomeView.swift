8f471c1f2237928503bc9442952753a80abc60f0
mengoreo <mengoreo@163.com>
2020-03-10 18:21:26 +0800

    WelcomeView 删除不必要的comments

--- HomeAssessment/Views/WelcomeView.swift
+++ HomeAssessment/Views/WelcomeView.swift
@@ -36,42 +36,6 @@ struct WelcomeView: View {
 }
 
 extension WelcomeView {
-//    func BUTTONS() -> some View {
-//        return VStack {
-//            Spacer()
-//            HStack {
-//                if viewModel.signInUpViewModal {
-//                    Button(action: {
-//                        self.viewModel.showWelcom()
-//                        self.viewModel.updateView()
-//                    }) {
-//                        Image(systemName: "chevron.left").font(.title).frame(height: 30)
-//
-//                    }.padding(.leading, 20)
-//                    .buttonStyle(CollapsableOutlineBackgroundStyle(collapsed: true))
-//                }
-//
-//                Button(action: {
-////                    self.viewModel.showSignInView()
-//                    if self.viewModel.signInUpViewModal {
-//                        self.signInUpView.viewModel.authorising()
-//                    } else {
-//                        self.viewModel.showSignInUpView()
-//
-//                    }
-//                    self.viewModel.updateView()
-//                }) {
-//                    Text("登录/注册")
-//                        .fontWeight(.semibold)
-//                        .font(.title)
-//                }
-//                .buttonStyle(CollapsableGradientBackgroundStyle())
-//
-//            }
-//        }
-//        .padding(.bottom, viewModel.uiProperties.buttonBottomPadding)
-//        .animation(.myease)
-//    }
     
     func ICON() -> some View {
                     
