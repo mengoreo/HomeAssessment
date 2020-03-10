5c4fe7b2eb4bf159c4f3d8e158765cfec5774821
mengoreo <mengoreo@163.com>
2020-03-10 18:16:24 +0800

    自定义拍照片视图

---
+++ HomeAssessment/Customisation/UIViewRepresentables/TakePhotos.swift
@@ -0,0 +1,67 @@
+//
+//  TakePhotos.swift
+//  HomeAssessment
+//
+//  Created by Mengoreo on 2020/3/8.
+//  Copyright © 2020 Mengoreo. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct CameraView: UIViewControllerRepresentable {
+    
+    var completionHandler: (_ capturedImage: UIImage?) -> Void
+    
+    init(completionHandler: @escaping (_ capturedImage: UIImage?) -> Void) {
+        self.completionHandler = completionHandler
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CameraView>) -> UIImagePickerController {
+        let imagePickerController = UIImagePickerController()
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePickerController.sourceType = .photoLibrary
+        } else {
+            imagePickerController.sourceType = .camera
+            imagePickerController.cameraCaptureMode = .photo
+        }
+//        imagePickerController.allowsEditing = true
+        
+        imagePickerController.delegate = context.coordinator
+        
+        return imagePickerController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<CameraView>) {
+//        uiViewController.
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: CameraView
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            picker.dismiss(animated: true)
+
+            guard let image = info[.originalImage] as? UIImage else {
+                print("No image found")
+                parent.completionHandler(nil)
+                return
+            }
+
+            // print out the image size as a test
+            parent.completionHandler(image)
+        }
+        
+//        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//            picker.dismiss(animated: true)
+//        }
+        
+    }
+}
