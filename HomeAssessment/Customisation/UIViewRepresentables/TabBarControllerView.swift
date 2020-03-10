b2e6f7e05f9849cc92851b07146f9b5bfebdd15c
mengoreo <mengoreo@163.com>
2020-03-10 18:15:17 +0800

    自定义 TabViewCtroller

---
+++ HomeAssessment/Customisation/UIViewRepresentables/TabBarControllerView.swift
@@ -0,0 +1,50 @@
+//
+//  TabBarControllerView.swift
+//  HomeAssessment
+//
+//  Created by Mengoreo on 2020/3/8.
+//  Copyright © 2020 Mengoreo. All rights reserved.
+//
+import SwiftUI
+import UIKit
+
+struct TabBarController: UIViewControllerRepresentable {
+    var controllers: [UIViewController]
+//    var selectedIndex: Binding<Int16>
+    @Binding var selectedIndex: Int16
+    @Binding var hideTabBar: Bool
+    func makeUIViewController(context: Context) -> UITabBarController {
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = controllers
+        tabBarController.delegate = context.coordinator
+        tabBarController.selectedIndex = Int(selectedIndex)
+        return tabBarController
+    }
+
+    func updateUIViewController(_ tabBarController: UITabBarController, context: Context) {
+        tabBarController.selectedIndex = Int(selectedIndex)
+        if hideTabBar {
+            tabBarController.tabBar.hide()
+        } else {
+            tabBarController.tabBar.show()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UITabBarControllerDelegate {
+        var parent: TabBarController
+
+        init(_ tabBarController: TabBarController) {
+            self.parent = tabBarController
+        }
+        
+        func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+            parent.selectedIndex = Int16(tabBarController.selectedIndex)
+        }
+    }
+}
+
+
