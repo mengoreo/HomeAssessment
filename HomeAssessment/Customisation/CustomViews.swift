efdbd649e3b0930cfc7cda87195d133f4706f048
mengoreo <mengoreo@163.com>
2020-03-10 18:16:59 +0800

    添加 ProgressBar: View 和 UIKitTabView: View

--- HomeAssessment/Customisation/CustomViews.swift
+++ HomeAssessment/Customisation/CustomViews.swift
@@ -76,4 +76,75 @@ struct ActionSheetTint: ViewModifier {
 }
 
 
+struct ProgressBar: View {
+    @Binding var value:CGFloat
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .trailing) {
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .opacity(0.1)
+                    Rectangle()
+                        .frame(minWidth: 0, idealWidth:self.getProgressBarWidth(geometry: geometry),
+                               maxWidth: self.getProgressBarWidth(geometry: geometry))
+//                        .opacity(0.5)
+                        .background(Color.accentColor)
+                        .animation(.myease)
+                }
+                .frame(height:5)
+            }.frame(height:5)
+        }
+    }
+    
+    func getProgressBarWidth(geometry:GeometryProxy) -> CGFloat {
+        let frame = geometry.frame(in: .global)
+        return frame.size.width * value
+    }
+}
+
+
+struct UIKitTabView: View {
+    var viewControllers: [UIHostingController<AnyView>]
+//    @State var selectedIndex: Int = 0
+    @Binding var selectedIndex: Int16
+    @Binding var hideTabBar: Bool
+    init(_ views: [Tab],
+         selectedIndex: Binding<Int16> = .constant(0),
+         hideTabBar: Binding<Bool> = .constant(false)
+    ) {
+        self.viewControllers = views.map {
+            let host = UIHostingController(rootView: $0.view)
+            host.tabBarItem = $0.barItem
+            return host
+        }
+        self._selectedIndex = selectedIndex
+        self._hideTabBar = hideTabBar
+        print("UIKitTabView: ", hideTabBar.wrappedValue)
+    }
+    
+    var body: some View {
+        TabBarController(controllers: viewControllers, selectedIndex: $selectedIndex, hideTabBar: $hideTabBar)
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    struct Tab {
+        var view: AnyView
+        var barItem: UITabBarItem
+        
+        init<V: View>(view: V, barItem: UITabBarItem) {
+            self.view = AnyView(view)
+            self.barItem = barItem
+        }
+        
+        
+        // convenience
+        init<V: View>(view: V, title: String?, image: String, selectedImage: String? = nil) {
+            let selectedImage = selectedImage != nil ? UIImage(systemName: image) : nil
+            let barItem = UITabBarItem(title: title, image: UIImage(systemName: image), selectedImage: selectedImage)
+            self.init(view: view, barItem: barItem)
+        }
+    }
+}
+
 
